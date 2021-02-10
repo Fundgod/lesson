b17c9002f44d859d30c83a1973e2375e5d02789e
andreygor444 <andreygor444@gmail.com>
2021-02-10 19:22:10 +0700

    Вторая задача

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ def format_coords(x, y):
 
 class App:
     def __init__(self):
-        self.spn = (0.005, 0.005)
+        self.spn = 0.001
         self.focus = (37.53, 55.7)
         self.map = self._load_map()
+        self._SPN_VALUES = (0.0005, 0.0007, 0.001, 0.0015, 0.003, 0.006, 0.1, 1, 2)
 
     def _load_map(self):
         params = {
             "ll": format_coords(*self.focus),
-            "spn": format_coords(*self.spn),
+            "spn": format_coords(self.spn, self.spn),
             "l": "map"
         }
         response = requests.get(MAP_API_SERVER, params=params)
@@ -36,8 +37,24 @@ class App:
         map = pygame.transform.scale(pygame.image.load('map.png'), SIZE)
         return map
 
+    def _change_spn(self, direction):
+        try:
+            self.spn = self._SPN_VALUES[self._SPN_VALUES.index(self.spn) + direction]
+            self.update()
+        except IndexError:  # уже установлено крайнее значение
+            return
+
+    def update(self):
+        self.map = self._load_map()
+
     def render(self, screen):
         screen.blit(self.map, (0, 0))
+
+    def on_keypress(self, key):
+        if key == pygame.K_PAGEDOWN:
+            self._change_spn(1)
+        elif key == pygame.K_PAGEUP:
+            self._change_spn(-1)
 
 
 def main():
@@ -50,8 +67,7 @@ def main():
         time.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                pass
-                # Тут будет какая-нибудь логика
+                app.on_keypress(event.key)
             elif event.type == pygame.QUIT:
                 running = False
         screen.fill(BLACK)
