8dd099dbc9a1a5147923f901f4ce7882b592d484
andreygor444 <andreygor444@gmail.com>
2021-02-10 19:33:32 +0700

    Третья задача

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ MAP_API_SERVER = "http://static-maps.yandex.ru/1.x/"
 FPS = 60
 BLACK = (0, 0, 0)
 SIZE = (600, 450)
+ARROW_BUTTONS = {pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT}
 
 
 def format_coords(x, y):
@@ -21,8 +22,9 @@ def format_coords(x, y):
 class App:
     def __init__(self):
         self.spn = 0.001
-        self.focus = (37.53, 55.7)
+        self.focus = [37.53, 55.7]
         self.map = self._load_map()
+        self.move_speed = 0.1
         self._SPN_VALUES = (0.0005, 0.0007, 0.001, 0.0015, 0.003, 0.006, 0.1, 1, 2)
 
     def _load_map(self):
@@ -44,6 +46,17 @@ class App:
         except IndexError:  # уже установлено крайнее значение
             return
 
+    def _change_focus(self, key):
+        if key == pygame.K_UP:
+            self.focus[1] += self.move_speed * self.spn
+        elif key == pygame.K_DOWN:
+            self.focus[1] -= self.move_speed * self.spn
+        elif key == pygame.K_LEFT:
+            self.focus[0] -= self.move_speed * self.spn
+        elif key == pygame.K_RIGHT:
+            self.focus[0] += self.move_speed * self.spn
+        self.update()
+
     def update(self):
         self.map = self._load_map()
 
@@ -55,6 +68,8 @@ class App:
             self._change_spn(1)
         elif key == pygame.K_PAGEUP:
             self._change_spn(-1)
+        elif key in ARROW_BUTTONS:
+            self._change_focus(key)
 
 
 def main():
